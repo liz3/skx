9e786ae654d0d2cdb3cff39c219ee30d3b437e7c
Liz3 <accs@liz3.net>
2020-07-16 03:19:55 +0200

    refactor

---
+++ include/api/PlayerEffects.h
@@ -0,0 +1,14 @@
+//
+// Created by liz3 on 16/07/2020.
+//
+
+#ifndef SKX_PLAYEREFFECTS_H
+#define SKX_PLAYEREFFECTS_H
+
+
+class PlayerEffects {
+
+};
+
+
+#endif //SKX_PLAYEREFFECTS_H
---
+++ src/api/PlayerEffects.cc
@@ -0,0 +1,5 @@
+//
+// Created by liz3 on 16/07/2020.
+//
+
+#include "../../include/api/PlayerEffects.h"
